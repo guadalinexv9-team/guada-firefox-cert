1ff10f275656fdf13ae649dc515d3091f18f0295
amatas <amatas@gmail.com>
2014-02-27 00:53:46 +0100

    Update scope.js

--- gcs/newfiles_skel/opt/firefox/browser/defaults/preferences/scope.js
+++ gcs/newfiles_skel/opt/firefox/browser/defaults/preferences/scope.js
@@ -3,3 +3,4 @@ pref("extensions.autoDisableScopes",0);
 pref("extensions.enabledScopes", 15);
 pref("app.update.auto", false);
 pref("app.update.channel", "");
+pref("browser.shell.checkDefaultBrowser", false);
